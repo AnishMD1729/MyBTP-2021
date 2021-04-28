9dc7cb4afad4bc76edb0dda69ad0f9c41421e1aa
AnishMD1729 <35302713+AnishMD1729@users.noreply.github.com>
2021-04-28 20:44:42 +0530

    Update selenium_ss.py

--- selenium_ss.py
+++ selenium_ss.py
@@ -33,78 +33,82 @@ with open('NoamChomsky.csv', 'wb') as f:
     w = csv.writer(f)
     w.writerows(abstract_dict.items())
 
-   def scrap_paper_list_by_title(self, paper_title_list: list):
+def scrap_paper_list_by_title(paper_title_list: list):
 
-        self._start_browser()
-        papers_dict = dict()
+    chrome_options = Options()
+    if self._headless:
+        chrome_options.add_argument("--headless")
 
-        for paper_name in tqdm.tqdm(paper_title_list):
-            try:
-                paper_dict = self.scrap_paper_by_title(
-                    paper_name, call_browser=False)
-                paper_id = paper_dict['paperId']
-                papers_dict[paper_id] = paper_dict
-            except KeyError:
-                pass
+    web_driver = webdriver.Chrome(chrome_options=chrome_options)
+    
+    papers_dict = dict()
 
-        self._close_browser()
+    for paper_name in tqdm.tqdm(paper_title_list):
+        try:
+            paper_dict = self.scrap_paper_by_title(
+                paper_name, call_browser=False)
+            papers_dict.append(paper_dict)
+        except KeyError:
+            pass
 
-        return papers_dict
+    web_driver.close()
 
-    def scrap_paper_by_title(self, paper_title: str, call_browser=True):
-        
-        attributes_dict = dict()
+    return papers_dict
 
-        if call_browser:
-            chrome_options = Options()
-            if headless:
-                chrome_options.add_argument("--headless")
+def scrap_paper_by_title(paper_title: str, call_browser=True):
 
-            web_driver = webdriver.Chrome(chrome_options=chrome_options)
+    attributes_dict = dict()
+
+    if call_browser:
+        chrome_options = Options()
+        if headless:
+            chrome_options.add_argument("--headless")
+
+        web_driver = webdriver.Chrome(chrome_options=chrome_options)
+
+    try:
+        web_driver.get(site_url)
+        try:
+            element_present = expected_conditions.presence_of_element_located((By.TAG_NAME, 'q'))
+            WebDriverWait(web_driver, timeout).until(element_present)
+        except TimeoutException:
+            raise
+        input_search_box = web_driver.find_element_by_name('q')
+        input_search_box.send_keys(paper_title)
+        input_search_box.send_keys(Keys.ENTER)
 
         try:
-            web_driver.get(site_url)
-            try:
-                element_present = expected_conditions.presence_of_element_located((By.TAG_NAME, 'q'))
-                WebDriverWait(web_driver, timeout).until(element_present)
-            except TimeoutException:
-                raise
-            input_search_box = web_driver.find_element_by_name('q')
-            input_search_box.send_keys(paper_title)
-            input_search_box.send_keys(Keys.ENTER)
-
-            try:
-                element_present = expected_conditions.presence_of_element_located((By.CLASS_NAME, 'search-result-title'))
-                WebDriverWait(web_driver, timeout).until(element_present)
-            except:
-                get_arxiv.left_titles(paper_title)
-            papers_div = web_driver.find_element_by_class_name('search-result-title')
-            first_paper_link = papers_div.find_element_by_tag_name('a')
-            first_paper_link.click()
-
-            try:
-                element_present = expected_conditions.presence_of_element_located((By.CLASS_NAME, 'mod-clckable'))
-                WebDriverWait(web_driver, timeout).until(element_present)
-            except TimeoutException:
-                raise
-
-            try:
-                more_button = web_driver.find_element_by_class_name('mod-clickable')
-                more_button.click()
-            except selenium.common.exceptions.ElementNotVisibleException:
-                pass
-
-            abstract_div = web_driver.find_element_by_class_name(
-                'text-truncator')
-            abstract_text = abstract_div.text
-
-            attributes_dict['abstract'] = abstract_text
-
-            if call_browser:
-                web_driver.close()
-
+            element_present = expected_conditions.presence_of_element_located((By.CLASS_NAME, 'search-result-title'))
+            WebDriverWait(web_driver, timeout).until(element_present)
         except:
+            get_arxiv.left_titles(paper_title)
+        papers_div = web_driver.find_element_by_class_name('search-result-title')
+        first_paper_link = papers_div.find_element_by_tag_name('a')
+        first_paper_link.click()
+
+        try:
+            element_present = expected_conditions.presence_of_element_located((By.CLASS_NAME, 'mod-clckable'))
+            WebDriverWait(web_driver, timeout).until(element_present)
+        except TimeoutException:
+            raise
+
+        try:
+            more_button = web_driver.find_element_by_class_name('mod-clickable')
+            more_button.click()
+        except selenium.common.exceptions.ElementNotVisibleException:
+            pass
+
+        abstract_div = web_driver.find_element_by_class_name(
+            'text-truncator')
+        abstract_text = abstract_div.text
+
+        attributes_dict['abstract'] = abstract_text
+
+        if call_browser:
             web_driver.close()
 
-        finally:
-            return attributes_dict
+    except:
+        web_driver.close()
+
+    finally:
+        return attributes_dict
